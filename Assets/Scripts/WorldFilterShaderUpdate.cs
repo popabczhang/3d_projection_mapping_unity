a502beef2c10452417a4e2d645c84b680d3372de
Ryan Yan Zhang <ryanz@mit.edu>
2018-10-12 22:45:46 -0400

    added world filter shader updater; composed good and bad world model (building and street)

--- Assets/Scripts/WorldFilterShaderUpdate.cs
+++ Assets/Scripts/WorldFilterShaderUpdate.cs
@@ -7,32 +7,30 @@ public class WorldFilterShaderUpdate : MonoBehaviour {
     // reference: https://assetstore.unity.com/packages/vfx/shaders/cross-section-66300
     
     public GameObject filterSlider;
-    public Vector3 normal;
-    public Vector3 position;
-
     public GameObject inputParentGO;
 
-    private Renderer rend;
-
-    void Start()
-    {
-        //rend = GetComponent<Renderer>();
-        //normal = plane.transform.TransformVector(new Vector3(0, 0, -1));
-        //position = plane.transform.position;
-        //UpdateShaderProperties();
-    }
     void Update()
     {
         //UpdateShaderProperties();
-    }
 
-    private void UpdateShaderProperties()
-    {
-        //normal = plane.transform.TransformVector(new Vector3(0, 0, -1));
-        //position = plane.transform.position;
-        //rend.material.SetVector("_PlaneNormal", normal);
-        //rend.material.SetVector("_PlanePosition", position);
-    }
+        // find all GO in inputParentGO and add to inputGOList
+        List<GameObject> inputGOList = new List<GameObject>();
+        Transform[] AllChildren = inputParentGO.GetComponentsInChildren<Transform>();
+        foreach (Transform Child in AllChildren)
+        {
+            if (Child.gameObject.GetComponent<Renderer>() != null)
+            {
+                inputGOList.Add(Child.gameObject);
+            }
+        }
 
+        // loop all GO in inputGOList
+        for (int i = 0; i < inputGOList.Count; i++)
+        {
+            Renderer rend = inputGOList[i].GetComponent<Renderer>();
+            Vector3 position = filterSlider.transform.position;
+            rend.material.SetVector("_FilterPosition", position);
+        }
+    }
 
 }
