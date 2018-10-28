6c411d65db2dfa1a132688ee63ef67e953e67159
Ryan Yan Zhang <ryanz@mit.edu>
2018-10-28 01:57:16 -0400

    WIP building instantiation

--- Assets/Scripts/BuildingInstantiator.cs
+++ Assets/Scripts/BuildingInstantiator.cs
@@ -5,10 +5,25 @@ using UnityEngine;
 public class BuildingInstantiator : MonoBehaviour {
 
     public GameObject[] buildingPositions;
+    public GameObject[] buildingPrefabsGood;
+    public GameObject[] buildingPrefabsBad;
     public ScanningDataParser scanningDataParser;
-    
-	void Start () {
-		
+
+    public GameObject[] bldgGOsGood = new GameObject[18];
+    public GameObject[] bldgGOsBad = new GameObject[18];
+
+    void Start () {
+
+        // instantiate buildings
+        for (int i = 0; i < scanningDataParser.grid.buildings.Count; i ++)
+        {
+            ScanningDataParser.Building b = scanningDataParser.grid.buildings[i];
+            if (b.type != -1 && buildingPrefabsGood[b.type] != null)
+            {
+                bldgGOsGood[i] = Instantiate(buildingPrefabsGood[b.type], buildingPositions[i].transform.position, buildingPositions[i].transform.rotation, gameObject.transform);
+                bldgGOsBad[i] = Instantiate(buildingPrefabsBad[b.type], buildingPositions[i].transform.position, buildingPositions[i].transform.rotation, gameObject.transform);
+            }
+        }
 	}
 	
 	void Update () {
