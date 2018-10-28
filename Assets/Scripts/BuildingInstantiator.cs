81a7b441e0930000dca3a17c930dc7bcdca56927
Ryan Yan Zhang <ryanz@mit.edu>
2018-10-28 01:00:40 -0400

    Updated: scanning data parser
    manual data parsing approach
    because native json utility doesn't support list of lists

---
+++ Assets/Scripts/BuildingInstantiator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingInstantiator : MonoBehaviour {
+
+    public GameObject[] buildingPositions;
+    public ScanningDataParser scanningDataParser;
+    
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/ScanningDataParser.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ScanningDataParser : MonoBehaviour {
+
+    public string ExampleJsonFilePath = "Assets/Resources/Example Building Scanning Data.txt";
+    public string JsonData;
+    public Grid grid = new Grid();
+
+    void Start () {
+        // Read the text from directly from the test.txt file
+        StreamReader reader = new StreamReader(ExampleJsonFilePath);
+        JsonData = reader.ReadToEnd();
+        Debug.Log(JsonData);
+        reader.Close();
+
+        // parsing json class
+        //scannedGrid = JsonUtility.FromJson<Grid>(JsonData); // native jsonUtility doesn't support list of lists
+        //Debug.Log(scannedGrid.grid);
+
+        // parsing json text manually
+        JsonData = JsonData.Replace(" ", "");
+        Debug.Log(JsonData);
+        int i1 = JsonData.IndexOf("{\"grid\":[[");
+        int i2 = JsonData.IndexOf("]]}");
+        string sub = JsonData.Substring(i1 + 10, i2 - (i1 + 10));
+        string[] subs = sub.Split(new string[] {"],["}, System.StringSplitOptions.None);
+        foreach (string s in subs)
+        {
+            string[] sp = s.Split(',');
+            Building b = new Building();
+            b.type = int.Parse(sp[0]);
+            b.rot = int.Parse(sp[1]);
+            grid.buildings.Add(b);
+        }
+        Debug.Log(grid);
+    }
+	
+	void Update () {
+
+    }
+
+    public class Grid
+    {
+        public List<Building> buildings = new List<Building>();
+    }
+
+    public class Building
+    {
+        public int type = -1;
+        public int rot = 0;
+    }
+
+}
