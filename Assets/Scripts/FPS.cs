ed891b57dfe67d430b17c48b53d2b67aeebc46cd
Ryan Yan Zhang <ryanz@mit.edu>
2018-11-05 15:19:50 -0500

    added fps display

---
+++ Assets/Scripts/FPS.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPS : MonoBehaviour {
+
+    public Text textFPS;
+
+    void Start()
+    {
+        textFPS = GetComponent<Text>();
+    }
+
+    void Update ()
+    {
+        if (Time.frameCount % 10 == 0)
+        {
+            textFPS.text = string.Format("{0} fps", (int)(1.0f / Time.deltaTime));
+        }
+	}
+}
