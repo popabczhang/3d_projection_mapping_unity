9ea8e3c7d03d2ce6d6aff215ac6ddc11f692baf5
Ryan Yan Zhang <ryanz@mit.edu>
2018-11-10 22:11:00 -0500

    Done: Projection mapping key pts calibration by dragging

---
+++ Assets/Scripts/KeyPointDrag.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyPointDrag : MonoBehaviour {
+
+    // ref: https://answers.unity.com/questions/12322/drag-gameobject-with-mouse.html
+
+    public Camera cam;
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    private float y;
+
+    //[RequireComponent(typeof(MeshCollider))]
+    void OnMouseDown()
+    {
+        //Debug.Log("OnMouseDown");
+
+        // record y(height) of the gameobject
+        y = transform.position.y;
+
+        screenPoint = cam.WorldToScreenPoint(transform.position);
+        //Debug.Log(string.Format("screenPoint: {0}.", screenPoint));
+
+        offset = transform.position - cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+
+    void OnMouseDrag()
+    {
+        //Debug.Log("OnMouseDrag");
+
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+
+        Vector3 curPosition = cam.ScreenToWorldPoint(curScreenPoint) + offset;
+
+        // apply curPosition and reset y to recorded value
+        transform.position = new Vector3(curPosition.x, y, curPosition.z);
+
+    }
+}
