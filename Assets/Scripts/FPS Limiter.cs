81303e1a13904505f282588adc303d58b26bd21f
City Science <cpgmedialab@gmail.com>
2018-11-16 14:38:18 -0500

    FPS Limiter

---
+++ Assets/Scripts/FPS Limiter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Limiter : MonoBehaviour
+{
+    public int targetFrameRate = 30;
+
+    private void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = targetFrameRate;
+    }
+}
