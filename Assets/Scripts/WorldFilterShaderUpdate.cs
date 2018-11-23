8f4131c9ec54cec1742519d776c20767bc1a3b21
Ryan Yan Zhang <ryanz@mit.edu>
2018-11-22 22:25:04 -0500

    Done: worldfilter follow box morph

--- Assets/Scripts/WorldFilterShaderUpdate.cs
+++ Assets/Scripts/WorldFilterShaderUpdate.cs
@@ -17,6 +17,8 @@ public class WorldFilterShaderUpdate : MonoBehaviour {
     public float xLeft = 0.37f;
     public float xRight = 1.53f;
 
+    public float sliderExtendRatio = 1.1429f; // extend ratio = length of bounding box of all roads / length of bounding box of all buildings
+
     void Update()
     {
         // auto moving mode for filter
@@ -58,18 +60,30 @@ public class WorldFilterShaderUpdate : MonoBehaviour {
             //Debug.Log(string.Format("meshParentGOName = {0}", meshParentGOName));
             string nameSuffix = meshParentGOName.Substring(meshParentGOName.Length - 4 - 7, 4); // name has "(Clone)" at the end
             //Debug.Log(string.Format("nameSuffix = {0}", nameSuffix));
+            Vector3 pt0 = position;
+            Vector3 pt1 = boxMorphKeyPts.TargetBoxKeyPts[3].transform.position * (1.0f - scanningSliderDataParser.sliderValue) + boxMorphKeyPts.TargetBoxKeyPts[2].transform.position * scanningSliderDataParser.sliderValue;
+            Vector3 pt4 = boxMorphKeyPts.TargetBoxKeyPts[4].transform.position * (1.0f - scanningSliderDataParser.sliderValue) + boxMorphKeyPts.TargetBoxKeyPts[5].transform.position * scanningSliderDataParser.sliderValue;
+            Vector3 pt5 = boxMorphKeyPts.TargetBoxKeyPts[7].transform.position * (1.0f - scanningSliderDataParser.sliderValue) + boxMorphKeyPts.TargetBoxKeyPts[6].transform.position * scanningSliderDataParser.sliderValue;
+            Vector3 pt2 = (pt4 + pt5) / 2f;
             Vector3 normal = new Vector3(1f, 1f, 1f);
             if (nameSuffix == "good")
             {
                 //Debug.Log("nameSuffix == \"good\"");
-                normal = new Vector3(1f, 0f, 0f);
+                normal = Vector3.Normalize(Vector3.Cross(pt1 - pt0, pt2 - pt0));
             }
             else
             {
                 //Debug.Log("nameSuffix != \"good\"");
-                normal = new Vector3(-1f, 0f, 0f);
+                normal = Vector3.Normalize(Vector3.Cross(pt1 - pt0, pt2 - pt0))*-1f;
             }
             //Debug.Log(string.Format("normal = {0}", normal));
+
+            // extend slider x, from boundary of building to boundary of road
+            float sliderExtendCenterX = (boxMorphKeyPts.OriginBoxKeyPts[0].transform.position.x + boxMorphKeyPts.OriginBoxKeyPts[1].transform.position.x) / 2f;
+            position = new Vector3((position.x - sliderExtendCenterX) * sliderExtendRatio + sliderExtendCenterX, position.y, position.z);
+            normal = new Vector3(normal.x * sliderExtendRatio, normal.y, normal.z);
+
+            // update shader
             rend.material.SetVector("_FilterPosition", position);
             rend.material.SetVector("_FilterNormal", normal);
         }
