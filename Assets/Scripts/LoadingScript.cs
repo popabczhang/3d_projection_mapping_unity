550315cd94295d248a78be23babb7d7d7edf8b8b
guadalupebabio <guadalupe.babio@gmail.com>
2018-11-26 18:22:47 -0500

    Slider try, text position modify

---
+++ Assets/Scripts/LoadingScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LoadingScript : MonoBehaviour {
+
+
+    public float scaleDelta = 0.2f;
+    public ScanningSliderDataParser scanningSliderDataParser;
+
+    private float sliderValue, x, y, z;
+    private Vector3 originalScale;
+
+    private void Start()
+    {
+        originalScale = transform.localScale;
+    }
+
+    void Update()
+    {
+
+        sliderValue = scanningSliderDataParser.sliderValue;
+
+        x = originalScale.x + (1f - sliderValue) * scaleDelta;
+        y = originalScale.y;
+        z = originalScale.z;
+        transform.localScale = new Vector3(x, y, z);
+
+    }
+}
