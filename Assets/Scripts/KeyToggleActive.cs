5bad087ddbf33e6bc53c697c86544c3f71de7bba
Ryan Yan Zhang <ryanz@mit.edu>
2018-11-30 14:27:23 -0500

    hide "UI" toggle and replaced by keyboard "u"

---
+++ Assets/Scripts/KeyToggleActive.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyToggleActive : MonoBehaviour {
+
+    public GameObject targetGO;
+    public string keyLetter = "u";
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyUp(keyLetter))
+        {
+            targetGO.SetActive(!targetGO.activeInHierarchy);
+        }
+
+    }
+
+
+}
